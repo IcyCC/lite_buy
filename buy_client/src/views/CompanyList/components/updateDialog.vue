75013d535df8d9ddf211369911c4e3e522356fb0
Chen kai <ck770701780@hotmail.com>
2020-04-04 16:54:35 +0800

    company管理的新增和修改的表单输入控制

--- buy_client/src/views/CompanyList/components/updateDialog.vue
+++ buy_client/src/views/CompanyList/components/updateDialog.vue
@@ -2,11 +2,11 @@
   <el-dialog title="添加公司" :visible="visible" @close="handleCancel">
     <el-form :model="data" ref="data" :rules="uploadRules" label-width="80px">
       <el-form-item label="公司名称" prop="name">
-        <el-input v-model="data.name" ref="name"></el-input>
+        <el-input v-model.trim="data.name" ref="name"></el-input>
       </el-form-item>
 
       <el-form-item label="公司法人" prop="owner">
-        <el-input v-model="data.owner" ref="owner"></el-input>
+        <el-input v-model.trim="data.owner" ref="owner"></el-input>
       </el-form-item>
 
       <el-form-item label="联系电话" prop="tele">
@@ -35,6 +35,7 @@
           :action="getUploadImageUrl()"
           :on-success="handleChange"
           :file-list="files"
+          accept="image/*"
           multiple>
           <i slot="default" class="el-icon-plus"></i>
           <div slot="file" slot-scope="{file}">
