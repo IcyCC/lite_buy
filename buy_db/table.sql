ad0c1d32ec98f15a560f28deebdb5ef0aee88927
Chen kai <ck770701780@hotmail.com>
2020-04-15 22:14:29 +0800

    数据库更改

--- buy_db/table.sql
+++ buy_db/table.sql
@@ -9,6 +9,7 @@ create table companys
   production_kind varchar(256) default '' not null,
   type            varchar(256)  default '' not null,
   imgs            TEXT          default '' not null,
+  productions	    TEXT          default '' not null,
   created_at      TIMESTAMP    default NULL,
   updated_at      TIMESTAMP    default NULL,
   deleted_at      TIMESTAMP    default NULL,
@@ -35,6 +36,8 @@ create table results
   company_id      int not null,
   type            varchar(32)  default '' not null,
   production_type varchar(32)  default '' not null,
+  content         TEXT          default '' not null,
+  comment   	    TEXT          default '' not null,
   created_at      TIMESTAMP    default NULL,
   updated_at      TIMESTAMP    default NULL,
   deleted_at      TIMESTAMP    default NULL,
@@ -42,3 +45,43 @@ create table results
   created_by      VARCHAR(255) default '' not null
 );
 
+CREATE TABLE experts (
+	id	INTEGER NOT NULL,
+	name	varchar(128) NOT NULL DEFAULT '',
+	tele	varchar(32) NOT NULL DEFAULT '',
+	note	varchar(512) NOT NULL DEFAULT '',
+	skilled_in	TEXT NOT NULL DEFAULT '',
+	created_at	TIMESTAMP DEFAULT NULL,
+	updated_at	TIMESTAMP DEFAULT NULL,
+	deleted_at	TIMESTAMP DEFAULT NULL,
+	updated_by	VARCHAR(255) NOT NULL DEFAULT '',
+	created_by	VARCHAR(255) NOT NULL DEFAULT '',
+	PRIMARY KEY(id)
+)
+
+CREATE TABLE productions (
+	id	INTEGER NOT NULL,
+	name	varchar(128) NOT NULL DEFAULT '',
+	detail	varchar(512) NOT NULL DEFAULT '',
+	production_kind	varchar(256) NOT NULL DEFAULT '',
+	imgs	TEXT NOT NULL DEFAULT '',
+	created_at	TIMESTAMP DEFAULT NULL,
+	updated_at	TIMESTAMP DEFAULT NULL,
+	deleted_at	TIMESTAMP DEFAULT NULL,
+	updated_by	VARCHAR(255) NOT NULL DEFAULT '',
+	created_by	VARCHAR(255) NOT NULL DEFAULT '',
+	PRIMARY KEY(id)
+)
+
+CREATE TABLE employments (
+	id	INTEGER NOT NULL,
+	expert_id	int NOT NULL,
+	content	TEXT NOT NULL DEFAULT '',
+	comment	TEXT NOT NULL DEFAULT '',
+	created_at	TIMESTAMP DEFAULT NULL,
+	updated_at	TIMESTAMP DEFAULT NULL,
+	deleted_at	TIMESTAMP DEFAULT NULL,
+	updated_by	VARCHAR(255) NOT NULL DEFAULT '',
+	created_by	VARCHAR(255) NOT NULL DEFAULT '',
+	PRIMARY KEY(id)
+)
